ae782dc8806b9f6ec99ea8e96ca9f07534da2f6a
beckybair <beckyhowen@gmail.com>
2018-09-18 22:28:19 -0500

    got gh-pages working and added google maps to display

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript(
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyDDg5y82_ZaWObvFIwt6jnWeieVguKQ918&callback=initMap'
+    );
+    window.initMap = this.initMap;
+  };
+
+  initMap = () => {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 44.625873, lng: -91.965471 },
+      zoom: 10
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <div id="map" />
+      </main>
     );
   }
+}
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName('script')[0];
+  var script = window.document.createElement('script');
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
