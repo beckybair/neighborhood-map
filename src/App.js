6b36fb6948e9600ca41721659b07212b832ab938
beckybair <beckyhowen@gmail.com>
2018-09-19 20:50:54 -0500

    added in Foursquare venues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
+    this.getVenues();
     this.renderMap();
   }
 
@@ -13,9 +21,31 @@ class App extends Component {
     window.initMap = this.initMap;
   };
 
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+    const params = {
+      client_id: 'FBDOAW2LIHLOKKUKCMGRODTGGJJ5H1T0TAAB2O520QD3DFPT',
+      client_secret: 'G2RDP3M0FWMKRPI353IVW3SILWKWTLZYD451RX3ADO411D3C',
+      query: 'coffee',
+      near: 'Eau Claire, WI',
+      v: '20180323'
+    };
+
+    axios
+      .get(endPoint + new URLSearchParams(params))
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        })
+      })
+      .catch(error => {
+        console.log('ERROR! ' + error);
+      });
+  };
+
   initMap = () => {
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 44.625873, lng: -91.965471 },
+      center: { lat: 44.810516, lng: -91.493506 },
       zoom: 10
     });
   };
